cf907c7d1d60f968807b84dc0e92b0553081d001
MauricioFCC <carvabmx@gmail.com>
2022-05-04 18:54:38 -0500

    Adicion de un nuevo archivo y modificacion del encabezado

--- RETO_1_UTP_OP_B.py
+++ RETO_1_UTP_OP_B.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Mauricio Carvajal grupo 05 UTP Telegram: @Mauro_Co
-# Uso de las listas y  tuplas en Python
+# Resolucion de un ploblema en Python
 
 # Valor intereses = cantidad $ CDT * 3% (0.03) * tiempo / 12
 # Valor total = Valor intereses + cantidad $ CDT
---
+++ RETO_1_UTP_OP_C.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Mauricio Carvajal grupo 05 UTP Telegram: @Mauro_Co
+# Resolucion de un ploblema en Python
