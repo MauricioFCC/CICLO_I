1f40ead763d10016a7fc3c0b2e91b23679465ca8
MauricioFCC <carvabmx@gmail.com>
2022-05-04 22:31:55 -0500

    Aregar tercera forma de resolver el reto

--- RETO_1_UTP_OP_C.py
+++ RETO_1_UTP_OP_C.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 # Mauricio Carvajal grupo 05 UTP Telegram: @Mauro_Co
 # Resolucion de un ploblema en Python
+
+####### Variables #######
+from functools import total_ordering
+
+
+usuario = 'usuario'
+capital = 1000000
+plazo = 12
+tiempo = 3
+interes = 0.03
+
+#CDT = capital * interes * tiempo / plazo
+#porcentajePerdida = capital * 0.02
+#valorTotal = CDT + capital
+#valorPerdida = round(capital - porcentajePerdida)
+#print(CDT("AB1012",1000000,3))
