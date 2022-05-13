08a30615e7673dd0890779cb53e2fbd44cc4c86e
MauricioFCC <carvabmx@gmail.com>
2022-05-12 23:00:00 -0500

    Ciclo 1 terminado

---
+++ unidad1/reto1utpFinal.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Mauricio Carvajal grupo 05 UTP Telegram: @Mauro_Co
+# Resolucion de un ploblema en Python ciclo 1
+
+def CDT (usuario, capital, tiempo):
+    if tiempo > 2: 
+        intereses = (capital * 0.03 * tiempo) / 12
+        valorTotal = intereses + capital
+    else:
+        porcentajePenal = capital * 0.02
+        valorTotal = round(capital - porcentajePenal)
+    
+    output = (f'Para el usuario {usuario} La cantidad de dinero a recibir, según el monto inicial {capital} para un tiempo de {tiempo} meses es: {valorTotal}')
+    
+    return output
+
+usuario = input('Ingrese Usuario: ')
+capital = int(input('Ingrese capital: '))
+tiempo = int(input('ingrse el tiempo en meses: '))
+output = CDT (usuario, capital, tiempo)
+print (output)
